9f2e9fab0e8c4ee0bae0c0b559ebc78190784947
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2024-03-28 16:27:32 +0330

    create controller

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -2,5 +2,26 @@ import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('songs')
 export class SongsController {
+  @Post()
+  create() {
+    return 'create a new song endpoint';
+  }
+  @Get()
+  findAll() {
+    return 'find all songs endpoint';
+  }
+  @Get(':id')
+  findOne() {
+    return 'fetch song on the based on id';
+  }
 
+  @Put(':id')
+  update() {
+    return 'update song on the based on id';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'delete song on the based on id';
+  }
 }
